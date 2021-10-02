3b1d1868869cf8e8997219c61160386269522f1e
pythaac <pythaac@gmail.com>
2021-10-03 07:56:36 +0900

    1) initial text changed ("blank" -> "...") 2) changed text to "..." if no data found 3) make textbox blank after adding problem

--- src/main/resources/static/js/question.js
+++ src/main/resources/static/js/question.js
@@ -5,12 +5,20 @@ function get_personality() {
         data: type,
         type:"POST",
         success: function (data) {
-            data = JSON.parse(data);
+            if (data){
+                data = JSON.parse(data);
 
-            $("#q_block_type").text(data.type);
-            $("#q_block_question").text(data.question);
-            $("#q_block_keyword").text(data.keyword);
-            $("#q_block_answer").text(data.answer);
+                $("#q_block_type").text(data.type);
+                $("#q_block_question").text(data.question);
+                $("#q_block_keyword").text(data.keyword);
+                $("#q_block_answer").text(data.answer);
+            }
+            else{
+                $("#q_block_type").text("...");
+                $("#q_block_question").text("...");
+                $("#q_block_keyword").text("...");
+                $("#q_block_answer").text("...");
+            }
         },
         error: function (e){
             console.log("get_personality error");
@@ -27,12 +35,20 @@ function get_skill() {
         contentType: "application/json; charset=utf-8;",
         type:"POST",
         success: function (data) {
-            data = JSON.parse(data);
+            if (data){
+                data = JSON.parse(data);
 
-            $("#q_block_type").text(data.type);
-            $("#q_block_question").text(data.question);
-            $("#q_block_keyword").text(data.keyword);
-            $("#q_block_answer").text(data.answer);
+                $("#q_block_type").text(data.type);
+                $("#q_block_question").text(data.question);
+                $("#q_block_keyword").text(data.keyword);
+                $("#q_block_answer").text(data.answer);
+            }
+            else{
+                $("#q_block_type").text("...");
+                $("#q_block_question").text("...");
+                $("#q_block_keyword").text("...");
+                $("#q_block_answer").text("...");
+            }
         },
         error: function (e){
             console.log("get_skill error");
@@ -49,12 +65,20 @@ function get_project() {
         contentType: "application/json; charset=utf-8;",
         type:"POST",
         success: function (data) {
-            data = JSON.parse(data);
+            if (data){
+                data = JSON.parse(data);
 
-            $("#q_block_type").text(data.type);
-            $("#q_block_question").text(data.question);
-            $("#q_block_keyword").text(data.keyword);
-            $("#q_block_answer").text(data.answer);
+                $("#q_block_type").text(data.type);
+                $("#q_block_question").text(data.question);
+                $("#q_block_keyword").text(data.keyword);
+                $("#q_block_answer").text(data.answer);
+            }
+            else{
+                $("#q_block_type").text("...");
+                $("#q_block_question").text("...");
+                $("#q_block_keyword").text("...");
+                $("#q_block_answer").text("...");
+            }
         },
         error: function (e){
             console.log("get_project error");
@@ -71,12 +95,20 @@ function get_all() {
         contentType: "application/json; charset=utf-8;",
         type:"POST",
         success: function (data) {
-            data = JSON.parse(data);
+            if (data){
+                data = JSON.parse(data);
 
-            $("#q_block_type").text(data.type);
-            $("#q_block_question").text(data.question);
-            $("#q_block_keyword").text(data.keyword);
-            $("#q_block_answer").text(data.answer);
+                $("#q_block_type").text(data.type);
+                $("#q_block_question").text(data.question);
+                $("#q_block_keyword").text(data.keyword);
+                $("#q_block_answer").text(data.answer);
+            }
+            else{
+                $("#q_block_type").text("...");
+                $("#q_block_question").text("...");
+                $("#q_block_keyword").text("...");
+                $("#q_block_answer").text("...");
+            }
         },
         error: function (e){
             console.log("get_all error");
@@ -101,6 +133,10 @@ function put_question() {
         success: function (data) {
             console.log(data);
             $("#saved").text(data);
+
+            $("#question").val("");
+            $("#keyword").val("");
+            $("#answer").val("");
         },
         error: function (e){
             console.log("put_question error");
